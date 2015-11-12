1a2b1f543e9eef8ee3f78a73dd8fa5a93997fc11
huangmubin <hmb87381041@163.com>
2015-11-12 16:27:10 +0800

    Add an extension to UIAlertController.
    Add an extension to UIAlertController.
    Add some create methods and add actions function.

---
+++ MUExtension.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+// MARK: - UIAlertController Extension
+
+extension UIAlertController {
+    
+    // MARK: Create Alert
+    
+    class func createSheet(title: String?, message: String?) -> UIAlertController {
+        return UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.ActionSheet)
+    }
+    class func createSheet(title: String?) -> UIAlertController {
+        return createSheet(title, message: nil)
+    }
+    class func createSheet() -> UIAlertController {
+        return createSheet(nil, message: nil)
+    }
+    class func createAlert(title: String?, message: String?) -> UIAlertController {
+        return UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+    }
+    class func createAlert(title: String?) -> UIAlertController {
+        return createAlert(title, message: nil)
+    }
+    class func createAlert() -> UIAlertController {
+        return createAlert(nil, message: nil)
+    }
+    
+    // MARK: Set Actions
+    
+    func addAction(title: String?, style: UIAlertActionStyle, handler: ((UIAlertAction) -> Void)?) {
+        let action = UIAlertAction(title: title, style: style, handler: handler)
+        self.addAction(action)
+    }
+    func addActions(titles: [String], handler: ((UIAlertAction) -> Void)?) {
+        for actionTitle in titles {
+            let action = UIAlertAction(title: actionTitle, style: UIAlertActionStyle.Default, handler: handler)
+            self.addAction(action)
+        }
+    }
+}
---
+++ MUTools.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class MUTools: NSObject {
+    
+}
